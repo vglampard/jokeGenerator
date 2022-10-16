49d25fc425185dc867044ff216e1a8889956b489
Victoria Lampard <vglampard@protonmail.com>
2022-10-16 14:12:15 +0100

    joke generator with image

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 // User presses button and dad joke appears
-// create 'get joke' button
-// add event listener with getJoke function that:
-    // gets new dad joke from api
-        // look at more complex fetch necessary
-        // data.json   
+// ✅ create 'get joke' button
+// ✅add event listener with getJoke function
+// getJoke FUNCTION:
+    // ✅ gets new dad joke from api
+        //✅  look at more complex fetch necessary
+        //✅  data.json   
     // prints it on the screen
-        // create <p> where joke will go 
-        // doc.WS it 
-        // add 'jokeLine.textContent = data.X
+        //✅ create <p> where joke will go 
+        //✅ doc.QS it 
+        //✅  add 'jokeLine.textContent = data.X
 
-        
+// IDENTIFYING ELEMENTS
+let jokeButton = document.querySelector("#jokeButton")
+let jokeLine = document.querySelector("#jokeLine")
 
-fetch("https://icanhazdadjoke.com/", {
-  headers: { accept: "application/json" },
-});
+jokeButton.addEventListener("click", getJoke)
+
+async function getJoke(){
+    let data = await fetch("https://icanhazdadjoke.com", {
+    headers: { accept: "application/json"},
+  });
+    let jokeData = await data.json();
+    jokeLine.textContent = jokeData.joke;
+}
+
